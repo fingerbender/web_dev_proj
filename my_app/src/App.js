2bb87957d250700ff12d905ff6b5cb4463e5d7b4
fingerbender <alan.yr.chien@gmail.com>
2024-12-03 07:42:25 -0500

    added react sample routes

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./templates/Home";
+import About from "./templates/About";
+import Contact from "./templates/Contact";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+        </a> */}
       </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
